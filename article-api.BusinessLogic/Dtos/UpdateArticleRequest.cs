df61b46363a6579768fe7e60921f12128d44d727
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 18:38:29 +0200

    Added object to update request

---
+++ article-api.BusinessLogic/Dtos/UpdateArticleRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace article_api.BusinessLogic.Dtos
+{
+    public class UpdateArticleRequest
+    {
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+    }
+}
