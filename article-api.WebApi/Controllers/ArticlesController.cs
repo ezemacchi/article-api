be39bf6923d2f81109703b73bad76bba3b99beca
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 12:14:33 +0200

    Added controller with hardcoded data to past the test, to make following refactors

---
+++ article-api.WebApi/Controllers/ArticlesController.cs
@@ -0,0 +1,25 @@
+﻿using article_api.WebApi.Dtos.CreateArticle;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace article_api.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ArticlesController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateArticle(CreateArticleRequest article)
+        {
+            var articleResponse = new CreateArticleResponse { Id = Guid.NewGuid(), Text = article.Text, Title = article.Title };
+
+            return CreatedAtAction(nameof(GetArticle), new { id= articleResponse.Id}, articleResponse);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetArticle(Guid id)
+        {
+            return Ok();
+        }
+    }
+}
