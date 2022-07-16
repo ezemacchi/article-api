c51bc3f2e4d68c9265d79f1f9534d442885e89f8
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 19:24:35 +0200

    added JsonIgnore to show properly on swagger

--- article-api.BusinessLogic/Dtos/UpdateArticleRequest.cs
+++ article-api.BusinessLogic/Dtos/UpdateArticleRequest.cs
@@ -1,7 +1,15 @@
-﻿namespace article_api.BusinessLogic.Dtos
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace article_api.BusinessLogic.Dtos
 {
     public class UpdateArticleRequest
     {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The Title is required")]
         public string Title { get; set; }
 
         public string Text { get; set; }
