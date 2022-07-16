1cff802cb18291efdae84938ad5fa7e20c5f8b60
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 13:01:07 +0200

    Added change on requestModel to manage badRequest case

--- article-api.WebApi/Dtos/CreateArticle/CreateArticleRequest.cs
+++ article-api.WebApi/Dtos/CreateArticle/CreateArticleRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace article_api.WebApi.Dtos.CreateArticle
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace article_api.WebApi.Dtos.CreateArticle
 {
     public class CreateArticleRequest
     {
+        [Required(ErrorMessage = "The Title is required")]
         public string Title { get; set; }
 
         public string Text { get; set; }
