3f97ea9eebc8261bbc9ee908fbc0d56d58dc86f3
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 18:22:41 +0200

    Added not found case for GetArticleById

--- article-api.IntegrationTests/ArticleTests.cs
+++ article-api.IntegrationTests/ArticleTests.cs
@@ -1,4 +1,4 @@
-using article_api.BusinessLogic.Dtos.CreateArticle;
+using article_api.BusinessLogic.Dtos;
 using article_api.DataAccess;
 using article_api.Domain.Models;
 using article_api.IntegrationTests.Configurations;
@@ -30,7 +30,7 @@ namespace article_api.IntegrationTests
             var articleRequest = new CreateArticleRequest { Title = "Some article", Text = "Hello World!" };
 
             var responseMessage = await httpClient.PostAsJsonAsync(Url, articleRequest);
-            var response = await responseMessage.Content.ReadFromJsonAsync<CreateArticleResponse>();
+            var response = await responseMessage.Content.ReadFromJsonAsync<ArticleDto>();
 
             var context = GetDbContext();
             var article = context.Articles.Find(response.Id);
@@ -42,8 +42,6 @@ namespace article_api.IntegrationTests
             Assert.Equal(articleRequest.Title, response.Title);
             Assert.Equal(articleRequest.Text, response.Text);
         }
-
-        
 
         [Fact]
         public async Task Create_New_Article_BadRequest()
@@ -63,15 +61,37 @@ namespace article_api.IntegrationTests
             var httpClient = _factory.CreateClient();
             var context = GetDbContext();
 
-            var id = Guid.NewGuid();
+            var id = Guid.Parse("7d34d51a-c3ad-4e92-8fd1-c69777026f43");
             var article = new Article { Id = id, Text = "Test text", Title = "Test title" };
             context.Articles.Add(article);
+            context.SaveChanges();
+            context.Dispose();
 
-            var responseMessage = await httpClient.GetAsync($"Url/{article.Id}");
-            var response = await responseMessage.Content.ReadFromJsonAsync<GetArticleResponse>();
+            var responseMessage = await httpClient.GetAsync($"{Url}/{article.Id}");
+            var response = await responseMessage.Content.ReadFromJsonAsync<ArticleDto>();
 
             Assert.Equal(article.Id, response.Id);
             Assert.Equal(HttpStatusCode.OK, responseMessage.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_Article_By_Id_NotFound()
+        {
+            var httpClient = _factory.CreateClient();
+            var context = GetDbContext();
+
+            var id = Guid.Parse("7d34d51a-c3ad-4e92-8fd1-c69777026f43");
+            var article = new Article { Id = id, Text = "Test text", Title = "Test title" };
+            context.Articles.Add(article);
+            context.SaveChanges();
+            context.Dispose();
+
+            var wrongId = Guid.Parse("7d5c7649-1fef-42ad-888f-350a12afc56a");
+            var responseMessage = await httpClient.GetAsync($"{Url}/{wrongId}");
+            var response = await responseMessage.Content.ReadAsStringAsync();
+
+            Assert.Equal(HttpStatusCode.NotFound, responseMessage.StatusCode);
+            Assert.Equal("Article not found", response);
         }
 
         private AppDbContext GetDbContext()
