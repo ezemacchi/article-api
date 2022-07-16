9c378480c5265fb2acbcb56147efc92dcae84140
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 13:52:46 +0200

    Added DataAccess project to handle queries to the database

---
+++ article-api.DataAccess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using article_api.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace article_api.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ article-api.DataAccess/Repositories/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using article_api.Domain.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace article_api.DataAccess.Repositories
+{
+    public interface IRepository
+    {
+        // Returns a found article or null.
+        Task<Article> Get(Guid id);
+
+        // Returns the identifier of a created article.
+        // Throws an exception if an article is null.
+        // Throws an exception if a title is null or empty.
+        Task<Guid> Create(Article article);
+
+        // Returns true if an article was deleted or false if it was not possible to find it.
+        Task<bool> Delete(Guid id);
+
+        // Returns true if an article was updated or false if it was not possible to find it.
+        // Throws an exception if an articleToUpdate is null.
+        // Throws an exception if a title is null or empty.
+        Task<bool> Update(Article articleToUpdate);
+    }
+}
