15fa5d146a248f8907fbb96f66efef4525c1428d
Ezequiel Macchi <macchimallo@gmail.com>
2022-07-16 11:35:47 +0200

    Added project Domain, to hold models and related data

---
+++ article-api.Domain/Models/Article.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace article_api.Domain.Models
+{
+    public class Article
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
